d112026bc7bdd971574788a7d74f51231605a826
DeftLuke <lukedean750@gmail.com>
2025-05-10 01:21:59 +0700

    Second Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-const title = "Sohoz88 - Bangladesh's Best Online Casino & Sports Betting Platform";
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
